72ff2dd5cc72f759423f45e461781eb21418cad0
Rupen97 <rupenpcb@gmail.com>
2025-11-06 18:21:50 +0545

    "Completed"

--- Program.cs
+++ Program.cs
@@ -1,107 +1,125 @@
-﻿//using System;
-
-//class Program
-//{
-//    public static void Main(string[] Args)
-//    {
-//        int[] num = { 8, 9, 3, 1, 5, 2};
-
-//        Console.WriteLine($"Sorting number");
-//        for (int i = 0; i < num.Length - 1; i++)
-//        {
-//            for (int j = 0; j < num.Length - i - 1; j++)
-//            {
-//                if (num[j] > num[j + 1])
-//                {
-//                    int temp = num[j];
-//                    num[j] = num[j + 1];
-//                    num[j + 1] = temp;
-//                }
-//            }
-//        }
-
-//        for (int i = 0; i < num.Length; i++)
-//        {
-//            Console.WriteLine(num[i]);
-//        }
-
-
-//        Console.WriteLine($"\nReverse number");
-
-//        for (int i =0; i < num.Length/2;  i++)
-//        {
-//            int temp = num[i];
-//            num[i] = num[num.Length - i - 1];
-//            num[num.Length -1 -i] = temp;
-//        }
-
-//        for (int i = 0; i < num.Length; i++)
-//        {
-//            Console.WriteLine(num[i]);
-//        }
-
-//        Console.WriteLine($"\nIndex finder");
-
-//        int number = 2;
-//        bool available = false;
-//        int index = -1;
-
-//        for (int i = 0;i < num.Length; i++)
-//        {
-//            if (num[i] == number)
-//            {
-//                available = true;
-//                index = i;
-//                break;
-//            }
-//        }
-
-//        if (available)
-//        {
-//            Console.WriteLine($"The number {number} is available at {index}");
-//        }
-//        else
-//        {
-//            Console.WriteLine($"The number {number} is not available in the array");
-//        }
-//    }
-//}
-
-
-//class Program
-//{
-//    public static void Main(string[] args)
-//    {
-//        DateTime birthDate = new DateTime(2005, 8, 11);
-//        DateTime current = DateTime.Now;
-//        TimeSpan age = current - birthDate;
-
-//        Console.WriteLine(age);
-
-//        int ageInYears = (int)(age.TotalDays / 365.25);
-
-
-//        Console.WriteLine($"Birthdate: {birthDate.ToShortDateString()}");
-//        Console.WriteLine($"Current date: {current.ToShortDateString()}");
-//        Console.WriteLine($"My age: {ageInYears}");
-
-//        DateTime newDate = birthDate.AddDays(10);
-//        Console.WriteLine($"Birthdate after adding 10 days: {newDate.ToShortDateString()}");
-//    }
-//}
-
-
-using System.Collections.Generic; 
-
+﻿
+using System.Collections.Generic;
 class Program
 {
-    public static void Main(string[] args)
+    // Task 1
+    public static void Task1()
     {
-       
+        String username = "Rupen";
+        int luckyNumber = 100;
+        Console.WriteLine($"Hello, {username}! Your lucky number is {luckyNumber}");
+    }
+
+    // Task 2
+    public double CalculateArea(double r)
+    {
+        return r * r * Circle.pi;
+    }
+
+    public double CalculateParameter(double r)
+    {
+        return 2 * r * Circle.pi;
+    }
+
+    public static void Task2()
+    {
+        Program program = new Program();
+
+        double radius = 5;
+        double area = program.CalculateArea(radius);
+        double parameter = program.CalculateParameter(radius);
+
+        Console.WriteLine($"The area of Circle with radius {radius} is {area}");
+        Console.WriteLine($"The parameter of Circle with radius {radius} is {parameter}");
+    }
+
+    // Task 3
+    public static void Task3()
+    {
+        byte b = 25;
+        short sh = 12345;
+        int i = 42;
+        long l = 1234567890;
+        float f = 1.12f;
+        double d = 1.12345;
+        decimal dem = 12345.6789m;
+        char c = 'C';
+        bool bol = true;
+        string s = "3.14";
+
+        string intToString = i.ToString();
+        double stringTodouble = Convert.ToDouble(s);
+
+        Console.WriteLine($"Byte: {b}");
+        Console.WriteLine($"Short: {sh}");
+        Console.WriteLine($"Int: {i}");
+        Console.WriteLine($"Long: {l}");
+        Console.WriteLine($"Float: {f}");
+        Console.WriteLine($"Double: {d}");
+        Console.WriteLine($"Decimal: {dem}");
+        Console.WriteLine($"Char: {c}");
+        Console.WriteLine($"Bool: {bol}");
+        Console.WriteLine($"Int to String: {intToString}");
+        Console.WriteLine($"String to Double: {stringTodouble}");
+    }
+
+    // Task 4
+ 
+    public static void Task4()
+    {
+        int[] num = { 8, 9, 3, 1, 5, 2 };
+
+        Console.WriteLine($"Sorting number");
+        Array.Sort(num);
+
+        foreach (int n in num)
+        {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine($"\nReverse number");
+        Array.Reverse(num);
+
+        foreach (int n in num)
+        {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine($"\nIndex finder");
+
+        int number = 2;
+        int index = Array.IndexOf(num, number);
+
+        if (index != -1)
+            Console.WriteLine($"The number {number} is available at {index}");
+        else
+            Console.WriteLine($"The number {number} is not available in the array");
+    }
+   
+
+    // Task 5
+    public static void Task5()
+    {
+        DateTime birthDate = new DateTime(2005, 8, 11);
+        DateTime current = DateTime.Now;
+        TimeSpan age = current - birthDate;
+
+        int ageInYears = (int)(age.TotalDays / 365.25);
+
+        Console.WriteLine($"Birthdate: {birthDate.ToShortDateString()}");
+        Console.WriteLine($"Current date: {current.ToShortDateString()}");
+        Console.WriteLine($"My age: {ageInYears}");
+
+        DateTime newDate = birthDate.AddDays(10);
+        Console.WriteLine($"Birthdate after adding 10 days: {newDate.ToShortDateString()}");
+    }
+
+    // Task 6
+    public static void Task6()
+    {
         List<string> fruits = new List<string>() { "Apple", "Banana", "Mango" };
 
         fruits.Add("Orange");
-
         fruits.Remove("Banana");
 
         Console.WriteLine("Fruits in the list:");
@@ -120,10 +138,30 @@ class Program
         fruitDictionary.Add(4, "Banana");
 
         Console.WriteLine("\nFruits in the dictionary:");
-
         foreach (KeyValuePair<int, string> i in fruitDictionary)
         {
             Console.WriteLine($"ID: {i.Key}, Name: {i.Value}");
         }
     }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Task 1");
+        Task1();
+        Console.WriteLine($"\nTask 2");
+        Task2();
+        Console.WriteLine($"\nTask 3");
+        Task3();
+        Console.WriteLine($"\nTask 4");
+        Task4(); 
+        Console.WriteLine($"\nTask 5");
+        Task5();
+        Console.WriteLine($"\nTask 6");
+        Task6();
+    }
+}
+
+class Circle
+{
+    public const double pi = 3.14;
 }
